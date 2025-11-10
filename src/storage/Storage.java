4ac4e16b423472af6b9aa5ec2ee10e7e8c02a537
kandevander <hanshenrik2300@gmail.com>
2025-11-10 11:44:29 +0100

    opgave 2

---
+++ src/storage/Storage.java
@@ -0,0 +1,44 @@
+package storage;
+
+import model.Forestilling;
+import model.Kunde;
+import model.Plads;
+
+import java.util.ArrayList;
+
+public class Storage {
+    private final ArrayList<Forestilling> forestillinger = new ArrayList<>();
+    private final ArrayList<Kunde> kunder = new ArrayList<>();
+    private final ArrayList<Plads> pladser = new ArrayList<>();
+
+
+    public void addForestillinger(Forestilling forestilling) {
+        if (!forestillinger.contains(forestilling)) {
+            forestillinger.add(forestilling);
+        }
+    }
+
+    public void addKunder(Kunde kunde) {
+        if (!kunder.contains(kunde)) {
+            kunder.add(kunde);
+        }
+    }
+
+    public void addPladser(Plads plads) {
+        if (!pladser.contains(plads)) {
+            pladser.add(plads);
+        }
+    }
+
+    public ArrayList<Forestilling> getForestillinger() {
+        return forestillinger;
+    }
+
+    public ArrayList<Kunde> getKunder() {
+        return kunder;
+    }
+
+    public ArrayList<Plads> getPladser() {
+        return pladser;
+    }
+}
